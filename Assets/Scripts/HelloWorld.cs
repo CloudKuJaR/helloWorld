324c3c43e84f265c2bfe08a382d59bee8ef97631
CloudKuJaR <cloudkujar1999@gmail.com>
2021-03-25 20:52:53 +0300

    Init project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    public int min = 1;
+    public int max = 1000;
+    private int guess;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Загадай число от " + min + " до " + max);
+        UpdateGuess();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            UpdateGuess();
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            UpdateGuess();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            print("game over");
+        }
+    }
+
+    private void UpdateGuess()
+    {
+        guess = (min + max) / 2;
+        print("Ты загадал число " + guess);
+    }
+
+
+
+}
